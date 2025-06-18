f524366cb7e1dd7897b7f935afdd9fb7e337eef9
Abhishek-Sirugudu <siruguduabhishek@gmail.com>
2025-06-18 21:57:40 +0530

    Create StringBuilderDemo.java

---
+++ 06-strings/StringBuilderDemo.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class SB {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Welcome");
+
+        str.append(" To Java");
+        str.insert(7, "Everyone");
+        System.out.println(str);
+        str.reverse();
+        System.out.println(str);
+
+
+    }
+}
